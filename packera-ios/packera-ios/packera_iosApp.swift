264b02f9e110d9c06f3cab4613fed613bd0a27dd
pinkysalvanera <salvanerapt@gmail.com>
2026-04-15 21:54:17 +0800

    Initial app creation

---
+++ packera-ios/packera-ios/packera_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  packera_iosApp.swift
+//  packera-ios
+//
+//  Created by Pinky Salvanera on 4/15/26.
+//
+
+import SwiftUI
+
+@main
+struct packera_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
